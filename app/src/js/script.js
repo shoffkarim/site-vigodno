93169e686230abc59ac47a23909ee79fa1be3607
shoffkarim <k4r1mch1k@gmail.com>
2021-06-25 12:55:37 +0500

    блок с проектами

--- app/src/js/script.js
+++ app/src/js/script.js
@@ -1,2 +1,10 @@
-// STYLES=============================================================================
+/* eslint-disable no-unused-vars */
+/* eslint-disable import/no-unresolved */
+/* eslint-disable no-undef */
 import '../sass/style.sass';
+
+let projects = new Swiper(".projects-slider", {
+  slidesPerView: 2.8,
+  spaceBetween: 30,
+  loop: false
+});
